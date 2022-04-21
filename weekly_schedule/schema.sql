0aa5c309ab276d9102ba52e7fc13ba32ab34a812
CANEVETGASPARD <canevet.gaspard@gmail.com>
2022-04-21 15:17:18 +0200

    start flask update by implementing the factory and db architecture. Next step will be be to design test for db

---
+++ weekly_schedule/schema.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS family;
+DROP TABLE IF EXISTS schedule;
+
+CREATE TABLE family (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  groupName TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE people (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  familyId INTEGER NOT NULL,
+  username TEXT NOT NULL,
+  monday INTEGER,
+  tuesday INTEGER,
+  wednesday INTEGER,
+  thursday INTEGER,
+  friday INTEGER,
+  saturday INTEGER,
+  sunday INTEGER,
+  FOREIGN KEY (familyId) REFERENCES family(id),
+  CONSTRAINT uniqueRelationPeopleFamily UNIQUE (username,familyId)
+);
+
+CREATE TABLE calendar (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  familyId INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  monday TEXT,
+  tuesday TEXT,
+  wednesday TEXT,
+  thursday TEXT,
+  friday TEXT,
+  saturday TEXT,
+  sunday TEXT,
+  FOREIGN KEY (familyId) REFERENCES family(id)
+);
