7f160e79f7938d184a92fdfe1db99c56bda474f3
CANEVETGASPARD <canevet.gaspard@gmail.com>
2022-05-19 22:38:59 +0200

    reorganised folder by adding src to static folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
     mode:"development",
     entry: {
-        "base": path.resolve(__dirname, 'weekly_schedule/src/base.ts'),
-        "home": path.resolve(__dirname, 'weekly_schedule/src/home.ts'),
-        "register": path.resolve(__dirname, 'weekly_schedule/src/register.ts'),
-        "login": path.resolve(__dirname, 'weekly_schedule/src/login.ts'),
+        "base": path.resolve(__dirname, 'weekly_schedule/static/src/base.ts'),
+        "home": path.resolve(__dirname, 'weekly_schedule/static/src/home.ts'),
+        "register": path.resolve(__dirname, 'weekly_schedule/static/src/register.ts'),
+        "login": path.resolve(__dirname, 'weekly_schedule/static/src/login.ts'),
     },
     module: {
         rules: [
