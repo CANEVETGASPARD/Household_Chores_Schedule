6ed6ff589b21ae0c4a7b9a59d7ee702a5227b582
CANEVETGASPARD <canevet.gaspard@gmail.com>
2022-06-20 15:13:55 +0200

    handling uniqueness within table model

--- weekly_schedule/schema.py
+++ weekly_schedule/schema.py
@@ -1,20 +1,22 @@
+from enum import unique
+from unicodedata import name
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
 class Family(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    family_name = db.Column(db.String(150))
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    family_name = db.Column(db.String(150),unique=True)
     password = db.Column(db.String(150))
     members = db.relationship("Member", backref="family_member", lazy=True)
     calendars = db.relationship("Calendar", backref="family_calendar", lazy=True)
 
 
 class Member(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True,unique=True)
     family_id = db.Column(db.Integer, db.ForeignKey("family.id"), nullable=False)
-    memberName = db.Column(db.String(150), nullable=False)
+    member_name = db.Column(db.String(150), nullable=False)
     monday = db.Column(db.Integer)
     tuesday = db.Column(db.Integer)
     wednesday = db.Column(db.Integer)
@@ -22,11 +24,12 @@ class Member(db.Model):
     friday = db.Column(db.Integer)
     saturday = db.Column(db.Integer)
     sunday = db.Column(db.Integer)
+    __table_args__ = (db.UniqueConstraint("family_id","member_name",name="family_member_uc"),)
 
 
 class Calendar(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    family_id = db.Column(db.Integer, db.ForeignKey("family.id"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True,unique=True)
+    family_id = db.Column(db.Integer, db.ForeignKey("family.id"), nullable=False,unique=True)
     created_date = db.Column(db.DateTime(timezone=True), default=func.now())
     title = db.Column(db.String(150), nullable=False)
     monday = db.Column(db.String(150))
