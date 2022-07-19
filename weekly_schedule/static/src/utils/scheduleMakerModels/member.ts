85b46686a8359dc7d90eb3a7910798babc733865
CANEVETGASPARD <canevet.gaspard@gmail.com>
2022-07-19 22:20:36 +0200

    add isAvailable function that check if a member is available given a special meal

--- weekly_schedule/static/src/utils/scheduleMakerModels/member.ts
+++ weekly_schedule/static/src/utils/scheduleMakerModels/member.ts
@@ -1,6 +1,8 @@
+import { MealInTheWeek } from "../customType";
+
 export class Member {
     private name:string;
-    private mealInTheWeek:[string,number][];
+    private mealInTheWeek:MealInTheWeek[];
     private minimumNumberOfTaskInTheWeek:number;
 
     constructor(memberData: any, numberOfTaskForTheMeal:number) {
@@ -13,7 +15,10 @@ export class Member {
                 this.name = memberData[memberDataKeyList[keyIndex]];
             } else {
                 if(memberData[memberDataKeyList[keyIndex]] != 0) {
-                    this.mealInTheWeek.push([memberDataKeyList[keyIndex],memberData[memberDataKeyList[keyIndex]]]);
+                    this.mealInTheWeek.push({
+                        "day":memberDataKeyList[keyIndex],
+                        "meal":memberData[memberDataKeyList[keyIndex]]
+                    });
                     if (memberData[memberDataKeyList[keyIndex]] == 3){
                         numberOfMealInTheWeek += 2; //member present both in meal and dinner => thus two meals
                     } else {
@@ -25,25 +30,35 @@ export class Member {
         this.minimumNumberOfTaskInTheWeek = numberOfMealInTheWeek/numberOfTaskForTheMeal;
     }
 
-    public getName(): string {
-        return this.name;
-    }
-
-    public getMealInTheWeek(): [string,number][] {
-        return this.mealInTheWeek;
-    }
-
-    public getMinimumNumberOfTaskInTheWeek(): number {
-        return this.minimumNumberOfTaskInTheWeek;
-    }
-
     public toString(): string{
         let output = "nom : " + this.getName() + "\n" + "repas : ";
         for (let mealIndex = 0;mealIndex<this.getMealInTheWeek().length;mealIndex++){
-            output += "meal on " + this.getMealInTheWeek()[mealIndex][0] + " : " + this.getMealInTheWeek()[mealIndex][1] + ", ";
+            output += "meal on " + this.getMealInTheWeek()[mealIndex].day + " : " + this.getMealInTheWeek()[mealIndex].meal + ", ";
         }
         output += "\n" + "minimum number of task in the week : " + this.getMinimumNumberOfTaskInTheWeek();
 
         return output
+    }
+
+    public isAvailable(studiedMealInTheWeek: MealInTheWeek): boolean {
+        let isAvailable: boolean = false;
+        for (let mealInTheWeekListIndex = 0; mealInTheWeekListIndex<this.getMealInTheWeek().length;mealInTheWeekListIndex++) {
+            if(this.getMealInTheWeek()[mealInTheWeekListIndex].day.toLowerCase() == studiedMealInTheWeek.day.toLowerCase() && (this.getMealInTheWeek()[mealInTheWeekListIndex].meal == studiedMealInTheWeek.meal || this.getMealInTheWeek()[mealInTheWeekListIndex].meal == 3)) {
+                isAvailable = true;
+            }
+        }
+        return isAvailable;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    private getMealInTheWeek(): MealInTheWeek[] {
+        return this.mealInTheWeek;
+    }
+
+    private getMinimumNumberOfTaskInTheWeek(): number {
+        return this.minimumNumberOfTaskInTheWeek;
     }
 }
