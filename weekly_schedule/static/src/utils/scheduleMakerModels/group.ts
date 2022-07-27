264b43439bfef05f9d6c5acaec76e26cef218141
CANEVETGASPARD <canevet.gaspard@gmail.com>
2022-07-27 17:13:29 +0200

    add findAvailableMember and findNewAvailableMember function and MealInTheWeek type to group class

--- weekly_schedule/static/src/utils/scheduleMakerModels/group.ts
+++ weekly_schedule/static/src/utils/scheduleMakerModels/group.ts
@@ -1,4 +1,5 @@
 import { Member } from "./member"
+import { MealInTheWeek } from "../customType";
 
 export class Group {
     private groupSize: number;
@@ -15,19 +16,39 @@ export class Group {
         }
     }
 
-    public getGroupSize(): number {
-        return this.groupSize;
-    }
-
-    public getMembers(): Member[] {
-        return this.Members;
-    }
-
     public toString(): string{
         let output: string = "number of members : " + this.getGroupSize() + "\n" + "Members details : ";
         for(let memberIndex: number = 0; memberIndex<this.getMembers().length;memberIndex++) {
             output += "\n" + this.getMembers()[memberIndex].toString();
         }
-        return output
+        return output;
+    }
+
+    public findAvailableMembers(studiedMealInTheWeek: MealInTheWeek): string[] {
+        let availableMembersWithinGroup: string[] = [];
+        for (let memberIndex = 0; memberIndex<this.getGroupSize();memberIndex++) {
+            if(this.getMembers()[memberIndex].isAvailable(studiedMealInTheWeek)) {
+                availableMembersWithinGroup.push(this.getMembers()[memberIndex].getName());
+            }
+        }
+        return availableMembersWithinGroup;
+    } 
+
+    public findNewAvailableMembers(previousAvailableMember:string, studiedMealInTheWeek:MealInTheWeek):string[] {
+        let differentAvailableMembersWithinGroup: string[] = [];
+        for (let memberIndex = 0; memberIndex<this.getGroupSize();memberIndex++) {
+            if(this.getMembers()[memberIndex].isAvailable(studiedMealInTheWeek) && this.getMembers()[memberIndex].getName() != previousAvailableMember) {
+                differentAvailableMembersWithinGroup.push(this.getMembers()[memberIndex].getName());
+            }
+        }
+        return differentAvailableMembersWithinGroup;
+    } 
+
+    private getGroupSize(): number {
+        return this.groupSize;
+    }
+
+    private getMembers(): Member[] {
+        return this.Members;
     }
 }
