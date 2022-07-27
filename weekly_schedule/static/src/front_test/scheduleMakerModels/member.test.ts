299c3d1c3dafc67d5f7a899aae377c975840c912
CANEVETGASPARD <canevet.gaspard@gmail.com>
2022-07-27 19:06:42 +0200

    finish test on member class

--- weekly_schedule/static/src/front_test/scheduleMakerModels/member.test.ts
+++ weekly_schedule/static/src/front_test/scheduleMakerModels/member.test.ts
@@ -15,35 +15,51 @@ const numberOfTaskForTheMeal: number = 2;
 const member: Member = new Member(memberData,numberOfTaskForTheMeal);
 
 describe("test on member object", function() {
-    test("Member should be available on monday diner", function() {
-        const mealInTheWeekMondayDiner: MealInTheWeek = {
+    test("Member should be available on monday lunch", function() {
+        const mealInTheWeekMondayLunch: MealInTheWeek = {
             "day":"monday",
             "meal":1
         }
-        expect(member.isAvailable(mealInTheWeekMondayDiner)).toEqual(true);
+        expect(member.isAvailable(mealInTheWeekMondayLunch)).toEqual(true);
     })
 
-    test("Member should be available on thursday diner", function() {
-        const mealInTheWeekThursdayDiner: MealInTheWeek = {
+    test("Member should be available on thursday lunch", function() {
+        const mealInTheWeekThursdayLunch: MealInTheWeek = {
             "day":"thursday",
             "meal":1
         }
-        expect(member.isAvailable(mealInTheWeekThursdayDiner)).toEqual(true);
+        expect(member.isAvailable(mealInTheWeekThursdayLunch)).toEqual(true);
     })
 
-    test("Member should not be available on saturday diner", function() {
-        const mealInTheWeekSaturdayDiner: MealInTheWeek = {
+    test("Member should be available on friday diner", function() {
+        const mealInTheWeekFridayDiner: MealInTheWeek = {
+            "day":"friday",
+            "meal":2
+        }
+        expect(member.isAvailable(mealInTheWeekFridayDiner)).toEqual(true);
+    })
+
+    test("Member should not be available on saturday lunch", function() {
+        const mealInTheWeekSaturdayLunch: MealInTheWeek = {
             "day":"saturday",
             "meal":1
         }
-        expect(member.isAvailable(mealInTheWeekSaturdayDiner)).toEqual(false);
+        expect(member.isAvailable(mealInTheWeekSaturdayLunch)).toEqual(false);
     })
 
-    test("Member should not be available on friday diner", function() {
-        const mealInTheWeekFridayDiner: MealInTheWeek = {
+    test("Member should not be available on friday lunch", function() {
+        const mealInTheWeekFridayLunch: MealInTheWeek = {
             "day":"friday",
             "meal":1
         }
-        expect(member.isAvailable(mealInTheWeekFridayDiner)).toEqual(false);
+        expect(member.isAvailable(mealInTheWeekFridayLunch)).toEqual(false);
+    })
+
+    test("Member should not be available on thursday diner", function() {
+        const mealInTheWeekThursdayDiner: MealInTheWeek = {
+            "day":"thursday",
+            "meal":2
+        }
+        expect(member.isAvailable(mealInTheWeekThursdayDiner)).toEqual(false);
     })
 })
