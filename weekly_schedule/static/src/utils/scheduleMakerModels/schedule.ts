4e74de0a8a99b32d61749e3da7b670474aa6d1bf
CANEVETGASPARD <canevet.gaspard@gmail.com>
2022-08-10 22:07:10 +0200

    add initScheduleRandomly function

--- weekly_schedule/static/src/utils/scheduleMakerModels/schedule.ts
+++ weekly_schedule/static/src/utils/scheduleMakerModels/schedule.ts
@@ -1,23 +1,54 @@
 import { Group } from "./group"
+import { randomIntegerGenerator } from "../randomIntegerGenarator";
+import { Member } from "./member";
+import { MealInTheWeek } from "../mealInTheWeek";
 
 export class Schedule {
    private numberOfMealPerDay: number;
    private numberOfTaskForTheMeal: number;
    private group: Group;
-   private schedule: string[][];
+   private schedule: string[][][];
 
-   constructor(numberOfMealPerDay: number, numberOfTaskForTheMeal:number, membersData: any) {
+   readonly DAYS = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+
+   constructor(membersData: any, numberOfMealPerDay: number, numberOfTaskForTheMeal:number) {
       this.numberOfMealPerDay = numberOfMealPerDay;
       this.numberOfTaskForTheMeal = numberOfTaskForTheMeal;
       this.group = new Group(membersData,numberOfTaskForTheMeal);
-      this.schedule = [[]]; //change this init with proper init => init with proper array row and column size
+      this.schedule = this.initScheduleRandomly(); //change this init with proper init => init with proper array row and column size
    }
 
-   public getNumberOfMealPerDay(): number {
+   private initScheduleRandomly(): string[][][] {
+      const NUMBEROFDAYPERWEEK: number = 7;
+      let schedule: string[][][] =  [];
+      for (let mealIndex: number = 0; mealIndex<this.getNumberOfMealPerDay();mealIndex++) {
+         let mealList: string[][]= []
+         for (let dayIndex: number = 0; dayIndex<NUMBEROFDAYPERWEEK;dayIndex++) {
+            let dayList: string[] = [];
+            let mealInTheWeek: MealInTheWeek = new MealInTheWeek(this.DAYS[dayIndex],mealIndex + 1); //+1 because 1=lunch, 2=diner
+            for (let taskIndex: number = 0;taskIndex<this.getNumberOfTaskForTheMeal();taskIndex++) {
+               let availableMembers: string[] = this.getGroup().findAvailableMembers(mealInTheWeek);
+               if (availableMembers.length == 0) {
+                  dayList = ["",""];
+               } else if (availableMembers.length == 1) {
+                  dayList = [availableMembers[0],availableMembers[0]];
+               } else {
+                  dayList = [availableMembers[randomIntegerGenerator(availableMembers.length)],availableMembers[randomIntegerGenerator(availableMembers.length)]];
+               }
+            }
+            mealList.push(dayList);
+         }
+      schedule.push(mealList);
+      }
+
+      return schedule
+   }
+
+   private getNumberOfMealPerDay(): number {
       return this.numberOfMealPerDay;
    } 
 
-   public getNumberOfTaskForTheMeal(): number {
+   private getNumberOfTaskForTheMeal(): number {
       return this.numberOfTaskForTheMeal;
    }
 
@@ -25,16 +56,16 @@ export class Schedule {
       return this.group;
    }
 
-   public getSchedule(): string[][] {
+   public getSchedule(): string[][][] {
       return this.schedule;
    }
 
-   public setTableau(newSchedule: string[][]) {
+   private setTableau(newSchedule: string[][][]) {
       this.schedule = newSchedule;// add verif for array size
    }
 
-   public setScheduleValue(rowIndex:number, columnIndex:number, newValue:string) {
+   private setScheduleValue(mealIndex:number, dayIndex:number, taskIndex:number, newValue:string) {
       //add verif for array size
-      this.schedule[rowIndex][columnIndex] = newValue
+      this.schedule[mealIndex][dayIndex][taskIndex] = newValue
    }
 }
